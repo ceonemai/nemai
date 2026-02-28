39a6bd8822838ac16b22023141ea3a7accdbca94
Cchanoknun <chanoknun.cho@gmail.com>
2026-02-28 15:36:18 +0700

    feat: backbone page

--- nemai/src/App.jsx
+++ nemai/src/App.jsx
@@ -3,26 +3,31 @@ import Hero from "./components/Hero/Hero";
 import Footer from "./components/Footer/Footer";
 import WhyNEM from "./components/WhyNEM/WhyNEM";
 import Mission from "./components/Mission/Mission";
+import Backbone from "./components/Backbone/Backbone";
 
 function App() {
   return (
-  <>
-    <Navbar />
+    <>
+      <Navbar />
 
-    <section id="hero">
-      <Hero />
-    </section>
+      <section id="hero">
+        <Hero />
+      </section>
 
-    <section id="why">
-      <WhyNEM />
-    </section>
+      <section id="why">
+        <WhyNEM />
+      </section>
 
-    <section id="mission">
-      <Mission />
-    </section>
+      <section id="mission">
+        <Mission />
+      </section>
 
-    <Footer />
-  </>
+      <section id="backbone">
+        <Backbone />
+      </section>
+
+      <Footer />
+    </>
   );
 }
 
